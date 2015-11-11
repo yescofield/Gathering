d51e208a1207525f33112c9b085a4d9eb875992b
yescofield <738127410@qq.com>
2015-11-11 19:14:29 +0800

    Revert "优化代码"
    This reverts commit 87e284680e9df4fb6eca96e6fceac3cab42c352c.

--- app/src/main/java/com/yezimm/gathering/MainActivity.java
+++ app/src/main/java/com/yezimm/gathering/MainActivity.java
@@ -62,28 +62,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        initMapAbout();
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    private void initMapAbout() {
         mMapView = (MapView) findViewById(R.id.MapView);
         mGpsButton = (ImageButton) findViewById(R.id.GpsButton);
         mMapCtrl = new MapWrapperController(mMapView);
@@ -133,6 +111,23 @@ public class MainActivity extends AppCompatActivity
         mMapCtrl.setEnableRotate('z', false);
         mMapCtrl.setEnableRotate('x', false);
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     public interface OnShowPopListener {
